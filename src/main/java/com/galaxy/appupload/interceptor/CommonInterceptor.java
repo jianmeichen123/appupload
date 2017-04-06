206b561753fcfdfb1f2cdec9347025e1e7f072ff
liuli <liuli@10.8.232.112>
2017-04-06 10:37:38 +0800

    拦截修改

--- src/main/java/com/galaxy/appupload/interceptor/CommonInterceptor.java
+++ src/main/java/com/galaxy/appupload/interceptor/CommonInterceptor.java
@@ -13,8 +13,10 @@ public class CommonInterceptor extends HandlerInterceptorAdapter{
             HttpServletResponse response, Object handler) throws Exception {
         //登录action
     	String loginUrl="/login/userLogin";
+    	//扫描二维码action
+    	String qrCodeUrl="/download/app";
     	//1、请求到登录页面 放行,不拦截登录action
-        if(request.getServletPath().startsWith(loginUrl)) {  
+        if(request.getServletPath().startsWith(loginUrl)||request.getServletPath().startsWith(qrCodeUrl)) {  
             return true;  
         }  
         //2、如果用户已经登录 放行    
